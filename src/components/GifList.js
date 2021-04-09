62df038c2fce790ddb13b07974528b8ee7f788ac
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-04-09 16:06:34 +1000

    Gif search working

---
+++ src/components/GifList.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function GifList(props) {
+        return (
+            <div>
+                {props.listOfGifs.map(
+                    gif => <img src={gif} key={gif} alt={`This is a giphy ${gif}`}/>
+                )}
+                
+            </div>
+        )
+}
+
+export default GifList
+
---
+++ src/containers/GifListContainer.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import GifSearch from '../components/GifSearch'
+import GifList from '../components/GifList'
+
+class GifListContainer extends React.Component {
+    
+        state = {
+            gifs: []
+        }
+
+        handleFetch = (searchVal) => {
+            const key = 'jdt5SxXfnmHb3h23Jguoieaz9mPflZOa'
+            const url = `https://api.giphy.com/v1/gifs/search?q=${searchVal}&api_key=${key}`
+            fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data.data[0].images.original.url)
+                const arrayOfGifs = data.data.map(gif => gif.images.original.url)
+                this.setState({
+                    gifs: arrayOfGifs.slice(0, 3)
+                })
+
+            })
+        
+        }
+
+        //componentDidMount
+
+        render() {
+            return (
+                <div>
+                    <GifSearch getResult={this.handleFetch}></GifSearch>
+                    <GifList listOfGifs={this.state.gifs}></GifList>
+                </div>
+            )
+        }
+
+}
+
+export default GifListContainer;
